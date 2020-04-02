7d2026d05e6f62bac8824c3176d7095b24a05327
dwarncke <daniel.warncke@gmail.com>
2020-04-02 19:43:18 +0000

    new places route

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,7 @@
 class PlacesController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'places#index'
   get 'about', to: 'pages#about'
+  resources :places
 end
